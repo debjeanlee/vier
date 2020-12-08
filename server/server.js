81df259883258d673353c870dadb6e5199f4a200
eskaine <eskaineliu@gmail.com>
2020-12-08 21:50:09 +0800

    implemented passport

--- server/server.js
+++ server/server.js
@@ -1,31 +1,23 @@
 require('dotenv').config();
-
 const express = require('express');
+const passport = require('./configs/passport.config');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-require('./config/mongo.config');
+require('./configs/mongo.config');
 
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static('dist'));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 
-/**
- * @routes
- * all routes should start with '/api'
- */
-
-// categories
 app.use('/api', require('./routes/index.routes'));
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
 });
-
-// // app.use('/api/one', require("./routes/one.routes"));
-// // app.use('/api/two', require("./routes/two.routes"));
-// // app.use('/api/three', require("./routes/three.routes"));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
