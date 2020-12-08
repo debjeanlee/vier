6d25e5378d31a94fd72132944e8f8069d63b10c0
debjeanlee <debsgenes@gmail.com>
2020-12-08 21:56:09 +0800

    updating session model

--- server/models/session.models.js
+++ server/models/session.models.js
@@ -13,15 +13,18 @@ const sessionSchema = new Schema({
   active: { type: Boolean, default: true },
   orders: [
     {
-      dish: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Dish',
-      },
-      quantity: { type: Number, default: 1, required: true },
-      progress: { type: Number, default: 0 },
+      items: [
+        {
+          dish: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Dish',
+          },
+          quantity: { type: Number, default: 1, required: true },
+          progress: { type: Number, default: 1, max: 5 },
+        },
+      ],
       totalCost: Number,
       orderedAt: { type: Date, default: Date.now },
-      overallDelivered: { type: Number, default: 0 },
     },
   ],
   cart: [
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,3 +2,5 @@ const router = require('express').Router();
 
 router.use('/categories', require('./categories.routes'));
 router.use('/dishes', require('./dishes.routes'));
+
+module.exports = router;
