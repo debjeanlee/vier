c4069ed99a0ecf3874f36149fd6ce3576a68267d
eskaine <eskaineliu@gmail.com>
2020-12-08 21:53:34 +0800

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const passport = require('./configs/passport.config');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 const app = express();
 
 require('./configs/mongo.config');
