ee8186beaf7b46cb59a242a75ff5e9faaded7d9d
eskaine <eskaineliu@gmail.com>
2020-12-08 21:43:07 +0800

    update index routes

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
 
+router.use('/auth', require('./auth.routes'));
 router.use('/categories', require('./categories.routes'));
 router.use('/dishes', require('./dishes.routes'));
+
+module.exports = router;
