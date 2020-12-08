669f52e12cc9e82bc500f37d5c062cf68d1fdf34
debjeanlee <debsgenes@gmail.com>
2020-12-08 23:06:36 +0800

    add session route

--- server/models/session.models.js
+++ server/models/session.models.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose;
 const sessionSchema = new Schema({
   sessionId: { type: Number, required: true, unique: true },
   startTime: { type: Date, default: Date.now },
-  endTime: { type: Date, default: Date.now },
+  endTime: { type: Date, default: undefined },
   table: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Table',
--- server/routes/sessions.routes.js
+++ server/routes/sessions.routes.js
@@ -1,7 +1,60 @@
 const router = require('express').Router();
 
-router.post('/new', (req, res) => {
-  res.send(201).json({ message: 'session created' });
+const Session = require('../models/session.models');
+
+// -------- get active sessions --------- need table schema?
+// router.get('/active', async (req, res) => {
+//   const active = await Session.find({ active: 'true' });
+//   res.status(200).json({ active });
+// });
+
+router.post('/new', async (req, res) => {
+  try {
+    const data = req.body;
+    const length = await Session.countDocuments({}, (err, count) => count);
+    const sessionId = length + 1;
+    const sessionData = {
+      table: data.table,
+      sessionId,
+    };
+    const newSession = new Session(sessionData);
+    await newSession
+      .save()
+      .then(() => {
+        res.status(201).json({ message: 'Session created' });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(401).json({ message: 'Something went wrong' });
+      });
+  } catch (error) {
+    res.status(401).json({ message: 'Something went wrong' });
+  }
 });
 
 module.exports = router;
+
+// sessionId: { type: Number, required: true, unique: true },
+//   startTime: { type: Date, default: Date.now },
+//   endTime: { type: Date, default: Date.now },
+//   table: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Table',
+//   },
+//   active: { type: Boolean, default: true },
+//   orders: [
+//     {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: 'Order',
+//     },
+//   ],
+//   cart: [
+//     {
+//       dish: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Dish',
+//       },
+//       quantity: { type: Number, default: 1, required: true },
+//     },
+//   ],
+// });
