c5ae6f42a606c15f30d2933413dd8a5640e1473e
debjeanlee <debsgenes@gmail.com>
2020-12-11 13:36:30 +0800

    add to cart route done

---
+++ server/routes/cart.routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+
+const Session = require('../models/session.models');
+const Order = require('../models/order.models');
+const Dish = require('../models/dish.models');
+
+/**
+ * ADD ITEM TO CART / UPDATE QTY IF ITEM EXISTS
+ * @method PATCH
+ * @params session id
+ * @body dish id
+ */
+router.patch('/add/:sessionid', async (req, res) => {
+  // find session
+  const session = await Session.findById(req.params.sessionid);
+  if (session.cart.length === 0) {
+    try {
+      session.cart.push({ dish: req.body.dishId });
+      await session.save();
+      res.status(200).json({ message: 'Item added to cart' });
+    } catch (err) {
+      res.status(400).json({ message: 'Something went wrong' });
+    }
+  } else {
+    const indexOfItem = session.cart.findIndex((el) => String(el.dish) === req.body.dishId);
+    // const item = session.cart.filter((el) => String(el.dish) === req.body.dishId);
+    if (indexOfItem !== -1) {
+      session.cart[indexOfItem].quantity += 1;
+      await session.save();
+      res.status(200).json({ message: 'Quantity of item increased' });
+    } else {
+      session.cart.push({ dish: req.body.dishId });
+      await session.save();
+      res.status(200).json({ message: 'Item added to cart' });
+    }
+  }
+});
+
+module.exports = router;
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 router.use('/auth', require('./auth.routes'));
+router.use('/cart', require('./cart.routes'));
 router.use('/categories', require('./categories.routes'));
 router.use('/dishes', require('./dishes.routes'));
 router.use('/session', require('./sessions.routes'));
