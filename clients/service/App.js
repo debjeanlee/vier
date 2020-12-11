ced559fb20ec28dd4007a7f959f0e47840b0e3fc
eskaine <eskaineliu@gmail.com>
2020-12-11 10:40:52 +0800

    service routes & pages

--- clients/service/App.js
+++ clients/service/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import PrivateRoute from '../shared/components/PrivateRoute';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return <div>Service</div>;
+  const [isAuth, setIsAuth] = useState(false);
+
+  return (
+    <BrowserRouter basename="/service">
+      <Switch>
+        <Route exact path="/">
+          {isAuth ? <Redirect to="/dashboard" /> : <Home />}
+        </Route>
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ clients/service/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Log from '../../shared/components/Log';
+
+function Home() {
+  return (
+    <div>
+      <Log />
+    </div>
+  );
+}
+
+export default Home;
