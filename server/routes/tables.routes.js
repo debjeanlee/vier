a0ebdaffdf853407657c767ac65c4d50b78a7250
debjeanlee <debsgenes@gmail.com>
2020-12-11 11:39:40 +0800

    removed console log

--- server/routes/tables.routes.js
+++ server/routes/tables.routes.js
@@ -10,7 +10,6 @@ const Table = require('../models/table.models');
 router.get('/empty', async (req, res) => {
   try {
     const tables = await Table.find({ sessionId: undefined });
-    console.log(tables);
     res.status(200).json({ tables });
   } catch (error) {
     res.status(400).json({ message: 'Something went wrong' });
