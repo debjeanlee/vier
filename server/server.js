c752da054d3feb4fb8cf0ded10fb837d562b492c
eskaine <eskaineliu@gmail.com>
2020-12-11 14:07:15 +0800

    implement socket backend

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const http = require('http');
 const passport = require('./configs/passport.config');
+const sockets = require('./helpers/sockets');
 
 const port = process.env.PORT;
 const app = express();
+const server = http.createServer(app);
 
 require('./configs/mongo.config');
 
@@ -13,12 +16,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
 
+sockets(server);
+
 app.use('/api', require('./routes/index.routes'));
 
 app.get('*', (req, res) => {
   res.sendStatus(404);
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
