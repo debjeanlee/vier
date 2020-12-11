9bb0b53c9539860b06904dd1c87b0dc5ae8d113e
debjeanlee <debsgenes@gmail.com>
2020-12-11 11:34:57 +0800

    get empty table routes added

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 router.use('/auth', require('./auth.routes'));
 router.use('/categories', require('./categories.routes'));
 router.use('/dishes', require('./dishes.routes'));
+router.use('/tables', require('./tables.routes'));
 router.use('/session', require('./sessions.routes'));
 
 module.exports = router;
---
+++ server/routes/tables.routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+const Table = require('../models/table.models');
+
+/**
+ * @method GET
+ * @route '/api/tables/empty'
+ * @returns list of empty tables
+ */
+router.get('/empty', async (req, res) => {
+  try {
+    const tables = await Table.find({ sessionId: undefined });
+    console.log(tables);
+    res.status(200).json({ tables });
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong' });
+  }
+});
+
+module.exports = router;
