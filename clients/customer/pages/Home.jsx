bbcea3b2696fbac494d0061c2a491225247ea71c
jnsw <jns.w@icloud.com>
2020-12-12 11:06:49 +0800

    home and menu scss

--- clients/customer/pages/Home.jsx
+++ clients/customer/pages/Home.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import Categories from '../components/Categories';
+import Dishes from '../components/Dishes';
 
-function Home() {
-  const categoryData = [
-    {
-      name: 'Starters',
-    },
-    {
-      name: 'Steak',
-    },
-    {
-      name: 'Combo',
-    },
-    {
-      name: 'Seafood',
-    },
-    {
-      name: 'Beverages',
-    },
-  ];
+function Home({setBackdropWidth, setSelectedCategory, selectedCategory}) {
+  const [categoriesData, setCategoriesData] = useState({});
+  const [categoryHeaderPos, setCategoryHeaderPos] = useState('100');
 
-  const categories = categoryData.map((category) => (
-    <div className="category-card">
-      <div className="category-info">
-        <div className="image-div" />
-        <h2>{category.name}</h2>
-        <p>7 items</p>
-      </div>
-      <div className="arrow-div">
-        <FontAwesomeIcon icon={faChevronRight} className="fa-arrow" />
-      </div>
-    </div>
-  ));
+  //   async function getCategories() {
+  //     try {
+  //       const res = await axios.get('https://localhost:3000/api/categories');
+  //       setCategoriesData(res.data);
+  //       console.log('data', categoriesData);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   getCategories();
 
-  return (
-    <div className="home-page-div">
-      <div className="categories-main-div">{categories}</div>
-    </div>
-  );
+  function openCategory(category) {
+    setSelectedCategory(category);
+    setBackdropWidth('70');
+    setCategoryHeaderPos('15');
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+  if (selectedCategory === '') {
+    return <Categories openCategory={openCategory} />;
+  }
+  return <Dishes selectedCategory={selectedCategory} categoryHeaderPos={categoryHeaderPos} />;
 }
 
 export default Home;
