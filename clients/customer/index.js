9fa334fb4bebb1839ad668c58de2dbb68e2b393a
eskaine <eskaineliu@gmail.com>
2020-12-12 17:25:27 +0800

    implement redux customer

--- clients/customer/index.js
+++ clients/customer/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from '../shared/helpers/store';
+import rootReducer from './reducers/rootReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store(rootReducer)}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
