e19addab7873f92baa77a42f4a1e759c331e0ac9
eskaine <eskaineliu@gmail.com>
2020-12-12 13:09:14 +0800

    update payment funcs

--- server/helpers/payment.js
+++ server/helpers/payment.js
@@ -2,6 +2,8 @@ const stripe = require('stripe')(process.env.STRIPE_SK);
 
 const newToken = () => stripe.terminal.connectionTokens.create();
 
+const capturePayment = async (paymentID) => stripe.paymentIntents.capture(paymentID);
+
 const initiatePayment = async (amount) => {
   const intent = await stripe.paymentIntents.create({
     amount,
@@ -16,4 +18,5 @@ const initiatePayment = async (amount) => {
 module.exports = {
   newToken,
   initiatePayment,
+  capturePayment,
 };
