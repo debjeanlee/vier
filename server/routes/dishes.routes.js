eb044e93595cd11c5ad6aea1eefa7681f9ca225a
debjeanlee <debsgenes@gmail.com>
2020-12-12 15:19:00 +0800

    get routes for menuitems

--- server/routes/dishes.routes.js
+++ server/routes/dishes.routes.js
@@ -3,6 +3,38 @@ const Dish = require('../models/dish.models');
 const Category = require('../models/category.models');
 
 /**
+ * GET SINGLE DISH VIA ID
+ * @method GET
+ * @route '/api/dishes/:dishId'
+ * @params dishId
+ * @returns info for single dish
+ */
+router.get('/:dishId', async (req, res) => {
+  try {
+    const dish = await Dish.findById(req.params.dishId).populate('category');
+    res.status(200).json({ dish });
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong' });
+  }
+});
+
+/**
+ * GETS LIST OF ALL DISHES
+ * @method GET
+ * @route '/api/dishes'
+ * @returns list of all dishes with categories populated
+ */
+router.get('/', async (req, res) => {
+  try {
+    const menuItems = await Dish.find().populate('category');
+    res.status(200).json({ menuItems });
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong' });
+  }
+});
+
+/**
+ * GET LIST OF DISHES BASED ON CATEGORY
  * @method GET
  * @route '/api/dishes/:category'
  * @returns list of starter dishes based on params
