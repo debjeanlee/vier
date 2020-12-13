118481ad928a896ea7a0834e6095ca7848838464
debjeanlee <debsgenes@gmail.com>
2020-12-13 17:05:47 +0800

    route to get ALL categories with items populated

--- server/routes/dishes.routes.js
+++ server/routes/dishes.routes.js
@@ -3,30 +3,30 @@ const Dish = require('../models/dish.models');
 const Category = require('../models/category.models');
 
 /**
- * GET SINGLE DISH VIA ID
- * @method GET
- * @route '/api/dishes/:dishId'
- * @params dishId
- * @returns info for single dish
- */
-router.get('/:dishId', async (req, res) => {
-  try {
-    const dish = await Dish.findById(req.params.dishId).populate('category');
-    res.status(200).json({ dish });
-  } catch (error) {
-    res.status(400).json({ message: 'Something went wrong' });
-  }
-});
-
-/**
  * GETS LIST OF ALL DISHES
  * @method GET
  * @route '/api/dishes'
- * @returns list of all dishes with categories populated
+ * @returns list of all dishes with categories populated sorted by categories
  */
 router.get('/', async (req, res) => {
   try {
-    const menuItems = await Dish.find().populate('category');
+    const allItems = await Dish.find().populate('category');
+    const starters = allItems.filter((item) => item.category.name === 'Starters');
+    const steak = allItems.filter((item) => item.category.name === 'Steak');
+    const combo = allItems.filter((item) => item.category.name === 'Combo');
+    const poultry = allItems.filter((item) => item.category.name === 'Poultry');
+    const others = allItems.filter((item) => item.category.name === 'Others');
+    const seafood = allItems.filter((item) => item.category.name === 'Seafood');
+    const dessert = allItems.filter((item) => item.category.name === 'Desserts');
+    const menuItems = [
+      { category: 'Starters', items: starters },
+      { category: 'Steak', items: steak },
+      { category: 'Combo', items: combo },
+      { category: 'Poultry', items: poultry },
+      { category: 'Others', items: others },
+      { category: 'Seafood', items: seafood },
+      { category: 'Dessert', items: dessert },
+    ];
     res.status(200).json({ menuItems });
   } catch (error) {
     res.status(400).json({ message: 'Something went wrong' });
@@ -49,6 +49,22 @@ router.get('/:category', async (req, res) => {
   } catch (error) {
     console.log(error);
     res.sendStatus(400);
+  }
+});
+
+/**
+ * GET SINGLE DISH VIA ID
+ * @method GET
+ * @route '/api/dishes/:dishId'
+ * @params dishId
+ * @returns info for single dish
+ */
+router.get('/:dishId', async (req, res) => {
+  try {
+    const dish = await Dish.findById(req.params.dishId).populate('category');
+    res.status(200).json({ dish });
+  } catch (error) {
+    res.status(400).json({ message: 'Something went wrong' });
   }
 });
 
