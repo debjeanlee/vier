8cde5d6afbb49c22858c5d0c2adf6ee8ed7c9404
debjeanlee <debsgenes@gmail.com>
2020-12-14 19:49:27 +0800

    orders cards expansion fixed

--- clients/customer/components/ui/OrderItemCard.jsx
+++ clients/customer/components/ui/OrderItemCard.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 function OrderItemCard({ order, number }) {
+  const [open, setOpen] = useState(false);
+
+  const orderDetails = order.items.map((el) => (
+    <div className="cart-item-card" key={el._id}>
+      <div className="cart-item-name-div">
+        <h6>{el.dish.name}</h6>
+      </div>
+      <div className="cart-quantity-price-div">
+        <div>x{el.quantity}</div>
+        <p>${el.dish.price * el.quantity}</p>
+      </div>
+    </div>
+  ));
+
+  function toggleOpen() {
+    setOpen(!open);
+  }
+
   return (
-    <div className="card">
-      <div className="card-header">
+    <div className={open === true ? 'card expand' : 'card'}>
+      <div className="card-header" onClick={() => toggleOpen()}>
         <h4>Order #{number + 1}</h4>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          className={open === true ? 'fa-arrow expand' : 'fa-arrow'}
+        />
       </div>
       <div>
         <p>Status: * change order schema? *</p>
         <div className="progress2 progress-moved">
           <div className="progress-bar2" />
         </div>
+        <div>{open ? orderDetails : ''}</div>
       </div>
     </div>
   );
