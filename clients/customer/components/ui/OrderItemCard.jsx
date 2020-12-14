c30dfe1a244abb94012c25431892860b3fa81907
debjeanlee <debsgenes@gmail.com>
2020-12-14 22:28:38 +0800

    completed show order page

--- clients/customer/components/ui/OrderItemCard.jsx
+++ clients/customer/components/ui/OrderItemCard.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 function OrderItemCard({ order, number }) {
   const [open, setOpen] = useState(false);
+  const [progress, setProgress] = useState(1);
+  const [progressBar, setProgressBar] = useState(<div className="progress-bar2" />);
 
   const orderDetails = order.items.map((el) => (
     <div className="cart-item-card" key={el._id}>
@@ -12,7 +14,7 @@ function OrderItemCard({ order, number }) {
         <h6>{el.dish.name}</h6>
       </div>
       <div className="cart-quantity-price-div">
-        <div>x{el.quantity}</div>
+        <p>x{el.quantity}</p>
         <p>${el.dish.price * el.quantity}</p>
       </div>
     </div>
@@ -21,6 +23,23 @@ function OrderItemCard({ order, number }) {
   function toggleOpen() {
     setOpen(!open);
   }
+
+  function getProgress() {
+    const confirmed = order.items.every((el) => el.progress === 2);
+    if (confirmed) {
+      setProgress(2);
+      setProgressBar(<div className="progress-bar2" style={{ width: '50%' }} />);
+    }
+    const complete = order.items.every((el) => el.progress === 5);
+    if (complete) {
+      setProgress(3);
+      setProgressBar(<div className="progress-bar2" style={{ width: '100%' }} />);
+    }
+  }
+
+  useEffect(() => {
+    getProgress();
+  }, []);
 
   return (
     <div className={open === true ? 'card expand' : 'card'}>
@@ -32,11 +51,16 @@ function OrderItemCard({ order, number }) {
         />
       </div>
       <div>
-        <p>Status: * change order schema? *</p>
+        <p>
+          Status: {progress === 1 && 'Pending'}
+          {progress === 2 && 'Preparing your food..'}
+          {progress === 3 && 'Completed'}
+        </p>
         <div className="progress2 progress-moved">
-          <div className="progress-bar2" />
+          {/* <div className="progress-bar2" /> */}
+          {progressBar}
         </div>
-        <div>{open ? orderDetails : ''}</div>
+        <div className="order-details-container">{open ? orderDetails : ''}</div>
       </div>
     </div>
   );
