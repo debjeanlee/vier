f2b3ef44b6ff9104d996d5bbd46a07d2eb56d547
jnsw <jns.w@icloud.com>
2020-12-14 23:51:11 +0800

    api func2

--- clients/service/components/ui/TableCard.jsx
+++ clients/service/components/ui/TableCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 import { axiosPatch, axiosPost } from '../../../shared/helpers/api';
 
 function TableCard({ tableData, getRestaurantData }) {
