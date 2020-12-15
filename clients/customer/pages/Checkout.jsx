8760cbda65418a878aa273789a4898688193aca5
eskaine <eskaineliu@gmail.com>
2020-12-15 17:06:47 +0800

    checkout socket

--- clients/customer/pages/Checkout.jsx
+++ clients/customer/pages/Checkout.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import socket from '../../shared/helpers/socket';
 
 function Checkout() {
   const { state } = useLocation();
+
+  function clickHandler() {
+    socket.transmit('checkout');
+  }
+
   return (
     <div>
       <h3>Checkout</h3>
       <div>Total amount payable: {state.total}</div>
-      <button>Request Payment</button>
+      <button onClick={clickHandler}>Request Payment</button>
     </div>
   );
 }
