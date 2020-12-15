39b4ae16d2cf7d60cdfab0f210a283333792431a
debjeanlee <debsgenes@gmail.com>
2020-12-15 10:40:08 +0800

    kitchen router moved

--- clients/kitchen/App.js
+++ clients/kitchen/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import './styles/kitchen.scss';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRoute from '../shared/components/PrivateRoute';
 import Home from '../shared/components/Home';
 import Dashboard from './pages/Dashboard';
@@ -9,7 +8,7 @@ function App() {
   const [isAuth, setIsAuth] = useState(false);
 
   return (
-    <BrowserRouter basename="/kitchen">
+    <>
       {/* TEMP SO DONT NEED TO KEEP LOGGING IN  */}
       {/* <Dashboard /> */}
       <Switch>
@@ -18,7 +17,7 @@ function App() {
         </Route>
         <PrivateRoute exact path="/dashboard" component={Dashboard} isAuth={isAuth} />
       </Switch>
-    </BrowserRouter>
+    </>
   );
 }
 
--- clients/kitchen/index.js
+++ clients/kitchen/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './styles/kitchen.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter basename="/kitchen">
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
